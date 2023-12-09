62466544ec68c603913afe1809fbb3d8eb70fb6e
bluehexagons <bluehexagons@users.noreply.github.com>
2023-12-08 19:08:02 -0600

    Add some security flags to see if it makes antivirus software happier?

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 rc antistatic.rc
-cl Antistatic.cpp antistatic.res /std:c++17 /EHsc /analyze /link /SUBSYSTEM:WINDOWS /RELEASE
+cl Antistatic.cpp antistatic.res /std:c++17 /EHsc /analyze /link /SUBSYSTEM:WINDOWS /RELEASE /GUARD:CF /NXCOMPAT
