7e821ddc423c7edf75f1f3a2f93c5be370be0705
bluehexagons <bluehexagons@users.noreply.github.com>
2023-12-10 16:37:20 -0600

    Reorganize

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # antistatic-loader
 Load Antistatic without all of those console windows popping up
+
+# building
+Run build.ps1
--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-rc antistatic.rc
-cl Antistatic.cpp antistatic.res /std:c++17 /EHsc /analyze /link /SUBSYSTEM:WINDOWS /RELEASE /GUARD:CF /NXCOMPAT
+rc /fo"build\antistatic.res" antistatic.rc
+cl /Fo"build\antistatic.obj" /Fe"bin\Antistatic.exe" "src\Antistatic.cpp" "build\antistatic.res" /std:c++17 /EHsc /analyze /link /SUBSYSTEM:WINDOWS /RELEASE /GUARD:CF /NXCOMPAT
