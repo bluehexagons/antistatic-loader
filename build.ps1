30ad640942b95c2a73eaceb85c0fb88aae3d2836
bluehexagons <bluehexagons@users.noreply.github.com>
2023-12-15 13:47:33 -0600

    Create build directories in the build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,5 @@
+New-Item -Path build -ItemType Directory -Force
+New-Item -Path bin -ItemType Directory -Force
+
 rc /fo"build\antistatic.res" antistatic.rc
 cl /Fo"build\antistatic.obj" /Fe"bin\Antistatic.exe" "src\Antistatic.cpp" "build\antistatic.res" /std:c++17 /EHsc /analyze /link /SUBSYSTEM:WINDOWS /RELEASE /GUARD:CF /NXCOMPAT
